33e6ec28b40f4c4fafe1cb25896418b8c1967acc
Jeremy Clements <jeremy.s.clements@gmail.com>
2024-02-25 20:22:44 -0700

    add <C-d>zz keymap to init

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,20 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		 require("neo-tree").setup({
+        event_handlers = {
+
+          {
+            event = "file_opened",
+            handler = function(file_path)
+              -- auto close
+              -- vimc.cmd("Neotree close")
+              -- OR
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+
+        }
+      })
 	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,5 @@ vim.cmd("set formatoptions-=cro")
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {})
